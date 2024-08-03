067d36323f438f5015fd12d881c08c7eeaf2da0e
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-08-03 23:07:23 +0300

    Add project files.

---
+++ Classes/Calisan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_1.Classes
+{
+    public class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public decimal Maas { get; set; }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Odev_1.Classes;
+
+GenelMudur genelMudur = new GenelMudur();
+genelMudur.Maas = 80000;
+
+Mudur mudur = new Mudur();
+mudur.Maas = 60000;
+
+Yazilimci yazilimci = new Yazilimci();
+yazilimci.Maas = 40000;
+
+Stajyer stajyer = new Stajyer();
+stajyer.Maas = 5000;
+
+List<Calisan> calisanlar = new List<Calisan>() { genelMudur, mudur,yazilimci, stajyer };
+
+decimal toplamMaas = 0;
+
+foreach ( var calisan in calisanlar)
+{
+    toplamMaas += calisan.Maas;
+}
+Console.WriteLine($"\nToplam Maas: {toplamMaas} TL");
